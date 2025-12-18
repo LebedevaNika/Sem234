69d185239cfc9259bfa8e8298a45d4813cb4e5b0
LebedevaNika <lnsnika@yandex.ru>
2025-12-18 03:38:09 +0300

    Create one_phase_merge_sort.h

---
+++ sem4/one_phase_merge_sort.h
@@ -0,0 +1,8 @@
+#ifndef ONE_PHASE_MERGE_SORT_H
+#define ONE_PHASE_MERGE_SORT_H
+
+// "Однофазная" сортировка (просто сортировка вставками)
+template <typename T>
+void one_phase_merge_sort(T arr[], int n);
+
+#endif
