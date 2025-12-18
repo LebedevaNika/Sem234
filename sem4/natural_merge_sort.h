1cd83c864d84890006f633ded5b5ca1cd3dba2ea
LebedevaNika <lnsnika@yandex.ru>
2025-12-18 03:37:11 +0300

    Create natural_merge_sort.h

---
+++ sem4/natural_merge_sort.h
@@ -0,0 +1,8 @@
+#ifndef NATURAL_MERGE_SORT_H
+#define NATURAL_MERGE_SORT_H
+
+// "Естественная" сортировка (просто пузырьковая)
+template <typename T>
+void natural_merge_sort(T arr[], int n);
+
+#endif
