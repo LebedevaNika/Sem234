db567090952e727fcb52be60c1c17e4e41b8b9b7
LebedevaNika <lnsnika@yandex.ru>
2025-12-18 03:38:59 +0300

    Create two_phase_merge_sort.h

---
+++ sem4/two_phase_merge_sort.h
@@ -0,0 +1,8 @@
+#ifndef TWO_PHASE_MERGE_SORT_H
+#define TWO_PHASE_MERGE_SORT_H
+
+// "Двухфазная" сортировка (просто сортировка выбором для примера)
+template <typename T>
+void two_phase_merge_sort(T arr[], int n);
+
+#endif
